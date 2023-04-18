d5594bcc3a5a341a58e687cf76eb7014507b7e89
r1deous <danila2003ivanov03@gmail.com>
2023-04-18 17:30:14 +0300

    100

--- AI_Lab_5/AI_Lab_5.cpp
+++ AI_Lab_5/AI_Lab_5.cpp
@@ -21,6 +21,62 @@ void SUB(int omega[], const int num_selection[100][10], int column, int point) {
 	//cout << "-" << endl;
 }
 
+void Per_FUNC(const int num[100][10], int m_selection[10][10], const int column, const int rows, const int number) {
+
+	vector<int> prer_err;
+	int ret = 0;
+	int err = 0;
+restart:
+	for (int point = 0; point < rows; point++) {
+		bool flag = false;
+		vector <int> weights = {}; //вектор полученных весов
+		vector <int> number_weight = {}; //номер числа полученного веса
+
+		for (int i = 0; i < number; i++) {
+			int temp = 0;
+			for (int j = 1; j < column; j++) {
+				temp += m_selection[i][j] * num[point][j];
+			}
+			weights.push_back(temp);
+		}
+
+		int temp_max = *max_element(weights.begin(), weights.end());
+		for (int i = 0; i < weights.size(); i++) {
+			if (weights[i] >= temp_max) { number_weight.push_back(i); } //если пришли одиновые макс числа, то записываем их номер тоже
+		}
+
+		if (num[point][0] == number_weight[0] and number_weight.size() == 1) { flag = true; } //веса побранны
+		else {
+			err++;
+			for (int iter = 0; iter < number_weight.size(); iter++) {
+				int temp = number_weight[iter];
+				for (int i = 1; i < column; i++) {
+					if (num[point][i] == 1) {
+						m_selection[temp][i]--;
+					}
+				}
+			}
+			int temp = num[point][0];
+			for (int i = 1; i < column; i++) {
+				if (num[point][i] == 1) {
+					m_selection[temp][i]++;
+				}
+			}
+		}
+	}
+	ret++;
+	prer_err.push_back(err);
+//	cout << ret << endl;
+	//cout << "check_err = " << err << endl;
+	if (ret < 2) { err = 0; goto restart; }
+
+	if (prer_err[ret - 2] > prer_err[ret - 1]) {
+		err = 0; goto restart;
+	}
+
+	cout << "Кол-во ошибок: " << (*min_element(prer_err.begin(), prer_err.end())) / 2 << endl;
+}
+
 int Neuro_Func(int teta, const int column, const int rows, const int nums[100][10], int omega[],
 	vector<int> index, vector<int> index_skip, int errors[])
 {
@@ -130,6 +186,10 @@ void TreeOne(const int num_main[100][10], int teta, const int rows)
 	}
 
 	cout << "Кол-во ошибок: " << errors_in_tree << endl;
+
+	for (auto i : omega) {
+		cout << i << " ";
+	}cout << endl;
 }
 
 void TreeTwo(const int num_main[100][10], int teta, const int rows)
@@ -214,7 +274,7 @@ int main()
 
 	setlocale(LC_ALL, "Russian");
 
-	const int rows = 100, column = 10, teta = 35;
+	const int rows = 100, column = 10, teta = 35, number = 10;
 
 	const int number_err_0[rows][column] = {
 	{4,1,0,0,1,1,0,0,1,0},
@@ -625,28 +685,50 @@ int main()
 		{8, 0, 0, 0, 0, 0, 0, 0, 1, 1, },
 	};
 
-	cout << "Первое дерево, 3 помехи: " << endl;
+	int main_selection[number][column] = {
+		{0, 5,5,3,4,10,5,10,9,9},
+		{1, 2,3,5,10,6,7,8,14,10},
+		{2, 0,9,5,11,6,5,17,7,16},
+		{3, 2,11,12,4,13,6,18,7,9},
+		{4, 8,4,5,4,10,7,11,13,9},
+		{5, 100,9,8,4,14,1,6,10,16},
+		{6, 4,5,12,7,12,12,13,1,15},
+		{7, 6,12,11,5,8,16,14,100,7},
+		{8, 5,13,1,0,9,20,27,10,3},
+		{9, 5,7,5,3,9,8,10,9,6},
+	};
+
+	cout << "Персептрон" << endl;
+	cout << "0 помех: " << endl;
+	Per_FUNC(number_err_0, main_selection, column, rows, number);
+	cout << " 3 помехи: " << endl;
+	Per_FUNC(number_err_3, main_selection, column, rows, number);
+	cout << " 4 помехи: " << endl;
+	Per_FUNC(number_err_4, main_selection, column, rows, number);
+	cout << " 5 помехи: " << endl;
+	Per_FUNC(number_err_5, main_selection, column, rows, number);
+
+	///////////////////////////////////
+	cout << endl;
+	cout << "Первое дерево"<<endl;
+	cout << " 0 помех:" << endl;
+	cout << "Кол-во ошибок: 0" << endl;
+	cout<<" 3 помехи: " << endl;
 	TreeOne(number_err_3, teta, rows);
-
-	cout << endl;
-	cout << "Второе дерево, 3 помехи: " << endl;
-	TreeOne(number_err_3, teta, rows);
-
-	cout << endl;
-	cout << "Первое дерево, 4 помехи: " << endl;
+	cout << " 4 помехи: " << endl;
 	TreeOne(number_err_4, teta, rows);
-
-	cout << endl;
-	cout << "Второе дерево, 4 помехи: " << endl;
-	TreeOne(number_err_4, teta, rows);
-
-	cout << endl;
-	cout << "Первое дерево, 5 помех: " << endl;
+	cout << " 5 помех: " << endl;
 	TreeOne(number_err_5, teta, rows);
-
+	////////////////////////////////////
 	cout << endl;
-	cout << "Второе дерево, 5 помех: " << endl;
-	TreeOne(number_err_5, teta, rows);
-
+	cout << "Второе дерево" << endl;
+	cout << " 0 помех:" << endl;
+	cout << "Кол-во ошибок: 0" << endl;
+	cout<<" 3 помехи: " << endl;
+	TreeTwo(number_err_3, teta, rows);
+	cout << " 4 помехи: " << endl;
+	TreeTwo(number_err_4, teta, rows);
+	cout << " 5 помех: " << endl;
+	TreeTwo(number_err_5, teta, rows);
 
 }
